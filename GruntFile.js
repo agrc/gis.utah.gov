16cc3aa600d7e972e70e0198ae3cc31f9ad61af6
steveoh <sgourley@utah.gov>
2016-01-28 11:24:35 -0700

    add image min

--- GruntFile.js
+++ GruntFile.js
@@ -16,11 +16,25 @@ module.exports = function (grunt) {
 				'!GruntFile.js',
 				'!package.json'
 			]
-		}
+		},
+		imagemin: {
+			main: {
+				options: {
+						optimizationLevel: 3
+					},
+					files: [{
+						expand: true, // Enable dynamic expansion
+						cwd: 'images/', // Src matches are relative to this path
+						src: ['**/*.{png,jpg,gif}'], // Actual patterns to match
+						dest: 'images/' // Destination path prefix
+					}]
+				}
+			}
 	});
 
 	// Dependencies
 	grunt.loadNpmTasks( 'grunt-gh-pages' );
+	grunt.loadNpmTasks( 'grunt-contrib-imagemin' );
 
-	grunt.registerTask( 'publish', [ 'gh-pages' ] );
+	grunt.registerTask( 'publish', [ 'imagemin:main', 'gh-pages' ] );
 };
