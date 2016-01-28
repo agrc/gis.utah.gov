0f3a73cd363bf0478f3d51ef8d30ba5560bb2c46
steveoh <sgourley@utah.gov>
2016-01-28 15:55:35 -0700

    try to make dev and github.io work

--- GruntFile.js
+++ GruntFile.js
@@ -4,9 +4,9 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 
-		'gh-pages': {
+ 		'gh-pages': {
 			options: {
-				base: '_site'
+				base: '_site/'
 			},
 		  src: [
 				'**/*',
@@ -19,7 +19,7 @@ module.exports = function (grunt) {
 		htmlmin: {
 			main: {
 				options: {
-					// removeComments: true,
+					removeComments: true,
 					// collapseWhitespace: true,
 					// conservativeCollapse: true
 					// removeEmptyAttributes: true,
@@ -29,60 +29,62 @@ module.exports = function (grunt) {
 				files: [{
 					expand: true,
           src: ['_site/**/*.html', '!_site/node_modules/**/*']
-			}]
-		}
-	},
-	imagemin: {
-		images: {
-			options: {
-					optimizationLevel: 3
-				},
-			files: [{
-				expand: true, // Enable dynamic expansion
-				cwd: 'images/', // Src matches are relative to this path
-				src: ['**/*.{png,jpg,jpeg,gif}'], // Actual patterns to match
-				dest: 'images/' // Destination path prefix
-			}]
-		},
-		downloads: {
-			options: {
-					optimizationLevel: 3
-				},
-			files: [{
-				expand: true, // Enable dynamic expansion
-				cwd: 'downloads/', // Src matches are relative to this path
-				src: ['**/*.{png,jpg,gif}'], // Actual patterns to match
-				dest: 'downloads/' // Destination path prefix
-			}]
-		}
-	},
-	replace: {
-    githubio: {
-      options: {
-        patterns: [{
-          match: /baseurl: ""/g,
-          replacement: 'baseurl: "/website"'
-        }, {
-					match: /url: \"\/\"/g,
-					replacement: 'url: "agrc.github.io"'
 				}]
-      },
-      src: '_config.yml',
-      dest: '_site/_config.yml'
-    }
-  }
-});
+			}
+		},
+		imagemin: {
+			images: {
+				options: {
+						optimizationLevel: 3
+					},
+				files: [{
+					expand: true, // Enable dynamic expansion
+					cwd: 'images/', // Src matches are relative to this path
+					src: ['**/*.{png,jpg,jpeg,gif}'], // Actual patterns to match
+					dest: 'images/' // Destination path prefix
+				}]
+			},
+			downloads: {
+				options: {
+						optimizationLevel: 3
+					},
+				files: [{
+					expand: true, // Enable dynamic expansion
+					cwd: 'downloads/', // Src matches are relative to this path
+					src: ['**/*.{png,jpg,gif}'], // Actual patterns to match
+					dest: 'downloads/' // Destination path prefix
+				}]
+			}
+		},
+		jekyll: {
+			githubio: {
+				options: {
+					config: '_config.yml,_config.github.io.yml'
+				}
+			},
+			main: {
+				options: {
+					serve: true,
+					auto: true
+				}
+			}
+		}
+	});
 
 	// Dependencies
 	grunt.loadNpmTasks('grunt-gh-pages');
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	grunt.loadNpmTasks('grunt-replace');
 	grunt.loadNpmTasks('grunt-newer');
 	grunt.loadNpmTasks('grunt-contrib-htmlmin');
+	grunt.loadNpmTasks('grunt-jekyll');
+
+	grunt.registerTask('default', [
+		'jekyll:main'
+	]);
 
 	grunt.registerTask('publish', [
 		'newer:imagemin',
-		'replace:githubio',
+		'jekyll:githubio',
 		'gh-pages'
 	]);
 };
