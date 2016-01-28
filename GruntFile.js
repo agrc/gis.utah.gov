b28ead5325ebc3d8cbda4f4779023b679aee72c7
steveoh <sgourley@utah.gov>
2016-01-28 12:12:51 -0700

    use replace on config
    this makes local dev and github.io work

--- GruntFile.js
+++ GruntFile.js
@@ -18,23 +18,55 @@ module.exports = function (grunt) {
 			]
 		},
 		imagemin: {
-			main: {
+			images: {
 				options: {
 						optimizationLevel: 3
 					},
-					files: [{
-						expand: true, // Enable dynamic expansion
-						cwd: 'images/', // Src matches are relative to this path
-						src: ['**/*.{png,jpg,gif}'], // Actual patterns to match
-						dest: 'images/' // Destination path prefix
-					}]
-				}
+				files: [{
+					expand: true, // Enable dynamic expansion
+					cwd: 'images/', // Src matches are relative to this path
+					src: ['**/*.{png,jpg,gif}'], // Actual patterns to match
+					dest: 'images/' // Destination path prefix
+				}]
+			},
+			downloads: {
+				options: {
+						optimizationLevel: 3
+					},
+				files: [{
+					expand: true, // Enable dynamic expansion
+					cwd: 'downloads/', // Src matches are relative to this path
+					src: ['**/*.{png,jpg,gif}'], // Actual patterns to match
+					dest: 'downloads/' // Destination path prefix
+				}]
 			}
+		},
+		replace: {
+      githubio: {
+        options: {
+          patterns: [{
+            match: 'baseurl: ""',
+            replacement: 'baseurl: "/website"'
+          }, {
+						match: 'url: "/"',
+						replacement: 'url: "agrc.github.io"'
+					}]
+        },
+        src: '_config.yml',
+        dest: '_site/_config.yml'
+      }
+    }
 	});
 
 	// Dependencies
-	grunt.loadNpmTasks( 'grunt-gh-pages' );
-	grunt.loadNpmTasks( 'grunt-contrib-imagemin' );
+	grunt.loadNpmTasks('grunt-gh-pages');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-replace');
+	grunt.loadNpmTasks('grunt-newer');
 
-	grunt.registerTask( 'publish', [ 'imagemin:main', 'gh-pages' ] );
+	grunt.registerTask('publish', [
+		'newer:imagemin',
+		'replace:githubio',
+		'gh-pages'
+	]);
 };
