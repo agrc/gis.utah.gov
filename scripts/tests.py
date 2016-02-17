9fc58435c8f803d6caeb5046282a9925e71999ab
steveoh <sgourley@utah.gov>
2016-02-16 19:35:03 -0700

    update tabs

--- scripts/tests.py
+++ scripts/tests.py
@@ -25,30 +25,30 @@ class TestCleanser(unittest.TestCase):
     pass
 
   def test_update_data_page_buttons(self):
-    test = '[button size="medium" color="white" textColor="#923922" link="ftp://ftp.agrc.utah.gov"]Download Locators[/button]'
+    test = '<a href="ftp://ftp.agrc.utah.gov" class="button medium white"><span class="button-text">Download Locators</span></a>'
     valid = '<a href="ftp://ftp.agrc.utah.gov" class="button medium white"><span class="button-text">Download Locators</span></a>'
 
     self.assertEqual(cleanser.update_data_download_button(test), valid)
 
   def test_captions(self):
-    test = '<p>[caption id="attachment_4794" align="alignright" width="256" caption="Concept of an Address Locator"]<img class="size-full wp-image-4794" src="{{ "/images/AddPointsGC2.png" | prepend: site.baseurl }}" alt="ChangeMeLarge"/>[/caption]</p>'
+    test = '<div class="caption"><img class="size-full wp-image-4794" src="{{ "/images/AddPointsGC2.png" | prepend: site.baseurl }}" alt="ChangeMeLarge"/><p class="caption-text">Concept of an Address Locator</p></div>'
     valid = '<div class="caption"><img class="size-full wp-image-4794" src="{{ "/images/AddPointsGC2.png" | prepend: site.baseurl }}" alt="ChangeMeLarge"/><p class="caption-text">Concept of an Address Locator</p></div>'
 
     self.assertEqual(cleanser.update_caption(test), valid)
 
-    test = '<p>[caption id="attachment_4794" align="alignright" width="256" caption=]<img class="size-full wp-image-4794" src="{{ "/images/AddPointsGC2.png" | prepend: site.baseurl }}" alt="ChangeMeLarge"/>[/caption]</p>'
+    test = '<div class="caption"><img class="size-full wp-image-4794" src="{{ "/images/AddPointsGC2.png" | prepend: site.baseurl }}" alt="ChangeMeLarge"/></div>'
     valid = '<div class="caption"><img class="size-full wp-image-4794" src="{{ "/images/AddPointsGC2.png" | prepend: site.baseurl }}" alt="ChangeMeLarge"/></div>'
 
     self.assertEqual(cleanser.update_caption(test), valid)
 
 
   def test_half_columns(self):
-    test = '''<p>[one_half]</p>
+    test = '''<div class="grid"><div class="grid__col grid__col--1-of-2">
     <h3>Overview</h3>
-    <p>[/one_half]</p>
-    <p>[one_half_last]</p>
+    </div>
+    <div class="grid__col grid__col--1-of-2">
     <h3>SGID Datasets</h3>
-    [/one_half_last]</p>'''
+    </div></div>'''
     valid = '''<div class="grid"><div class="grid__col grid__col--1-of-2">
     <h3>Overview</h3>
     </div>
@@ -59,51 +59,86 @@ class TestCleanser(unittest.TestCase):
     self.assertEqual(cleanser.update_columns(test), valid)
 
   def test_divider(self):
-    test = '<p>[divider_advanced color="#C36E00" paddingTop="2" paddingBottom="2"]</p>'
+    test = '<div class="divider"></div>'
     valid = '<div class="divider"></div>'
 
     self.assertEqual(cleanser.update_divider(test), valid)
 
-    test = '<p>[divider_padding]<br />'
+    test = '<div class="divider-padding"></div>'
     valid = '<div class="divider-padding"></div>'
 
     self.assertEqual(cleanser.update_divider(test), valid)
 
-    test = '<p>[divider_padding]</p>'
+    test = '<div class="divider-padding"></div>'
 
     self.assertEqual(cleanser.update_divider(test), valid)
 
   def test_icons(self):
-    test = '[icon_link style="download" color="gray" href="Link" target="_self"]<a title="Download available HRO imagery" href="http://raster.utah.gov/?catGroup=HRO%202012%20(12.5cm),HRO%202012%20(1ft),HRO%202009%20(25cm),HRO%202006%20(25cm),HRO%202003%20(30cm)&title=Utah%20HRO%20Imagery">HRO Orthophotography Interactive Map</a>[/icon_link]<br />'
+    test = '<span class="icon-text icon-download"><a title="Download available HRO imagery" href="http://raster.utah.gov/?catGroup=HRO%202012%20(12.5cm),HRO%202012%20(1ft),HRO%202009%20(25cm),HRO%202006%20(25cm),HRO%202003%20(30cm)&title=Utah%20HRO%20Imagery">HRO Orthophotography Interactive Map</a></span><br />'
     valid = '<span class="icon-text icon-download"><a title="Download available HRO imagery" href="http://raster.utah.gov/?catGroup=HRO%202012%20(12.5cm),HRO%202012%20(1ft),HRO%202009%20(25cm),HRO%202006%20(25cm),HRO%202003%20(30cm)&title=Utah%20HRO%20Imagery">HRO Orthophotography Interactive Map</a></span><br />'
 
     self.assertEqual(cleanser.update_icons(test), valid)
 
-    test = '<p>[icon style="calendar" color="gray"]Last Update: March, 2013[/icon]<br />'
+    test = '<p><span class="icon-text icon-calendar">Last Update: March, 2013</span><br />'
     valid = '<p><span class="icon-text icon-calendar">Last Update: March, 2013</span><br />'
 
     self.assertEqual(cleanser.update_icons(test), valid)
 
   def test_tables(self):
-    test = '<p>[styled_table]<br />'
+    test = ' '
     valid = ' '
 
     self.assertEqual(cleanser.update_tables(test), valid)
 
-    test = '[styled_table]</p>'
+    test = '<div class="table-style">'
     valid = '<div class="table-style">'
 
     self.assertEqual(cleanser.update_tables(test), valid)
 
-    test = '<p>[styled_table]</p>'
+    test = '<div class="table-style">'
 
     self.assertEqual(cleanser.update_tables(test), valid)
 
-    test = '[styled_table]</p>'
+    test = '<div class="table-style">'
 
     self.assertEqual(cleanser.update_tables(test), valid)
 
-    test = '<p>[/styled_table]</p>'
+    test = '</div>'
     valid = '</div>'
 
     self.assertEqual(cleanser.update_tables(test), valid)
+
+  def test_tabs(self):
+        test = '<p><br />'
+        valid = '<p><br />'
+        self.assertEqual(cleanser.update_tabs(test), valid, msg='1')
+
+        test = '<p><br />'
+        self.assertEqual(cleanser.update_tabs(test), valid, msg='2')
+
+        test = '<p></p>'
+        valid = '<p></p>'
+        self.assertEqual(cleanser.update_tabs(test), valid, msg='3')
+
+        test = '<p><br />'
+        valid = '<p><br />'
+        self.assertEqual(cleanser.update_tabs(test), valid, msg='4')
+
+        test = '<p> </p>'
+        valid = '<p> </p>'
+        self.assertEqual(cleanser.update_tabs(test), valid)
+
+        test = '<p></p>'
+        valid = '<p></p>'
+        self.assertEqual(cleanser.update_tabs(test), valid, msg='6')
+
+        test = 'This dataset was last updated in 2002.</p>'
+        valid = 'This dataset was last updated in 2002.</p>'
+        self.assertEqual(cleanser.update_tabs(test), valid)
+
+        test = '<h5 class="tab-title">Contact</h5><br />'
+        valid = '<h5 class="tab-title">Contact</h5>'
+        self.assertEqual(cleanser.update_tabs(test), valid)
+
+        test = '<h5 class="tab-title">Contact</h5><br />'
+        self.assertEqual(cleanser.update_tabs(test), valid)
