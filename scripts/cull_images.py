f4a73860155f93b1ce05d941808ab48603ab8994
steveoh <sgourley@utah.gov>
2016-03-31 14:51:04 -0600

    the script to :put_litter_in_its_place:

--- scripts/cull_images.py
+++ scripts/cull_images.py
@@ -1,47 +1,57 @@
 import os
 import sys
 import re
+import pprint
+import shutil
 
-image_extensions = ['.png_backup', '.jpg', '.xml', '.idx', '.jpeg',
-                    '.htm', '.bak', '.sample', '.gif', '.jpg_backup',
-                    '.scss', '.svg', '.pack', '.doc', '.bmp', '.js',
-                    '.png', '.json', '.css', '.m4v', '.ico', '.txt',
-                    '.html', '.pdf']
+known_extensions = ['.png_backup', '.jpg', '.xml', '.idx', '.jpeg',
+                    '.bak', '.sample', '.gif', '.jpg_backup', '.svg', '.pack',
+                    '.doc', '.bmp', '.png', '.css', '.m4v', '.ico',
+                    '.txt', '.pdf']
+pattern = '["\'][/]?((?:images|downloads)+[^"\']+?(?:{}))["\'?]'.format('|'.join(known_extensions))
 
 
 def one_offs(walk_dir):
     print('walk_dir = ' + walk_dir)
     print('walk_dir (absolute) = ' + os.path.abspath(walk_dir))
     files_on_disk = set([])
+    things_being_used = set([])
 
     for root, subdirs, files in os.walk(walk_dir):
-        if (root.startswith(walk_dir + '_site') or
-                root.startswith(walk_dir + 'node_modules') or
-                root.startswith(walk_dir + 'bower_components') or
-                root.startswith(walk_dir + '_sass') or
-                root.startswith(walk_dir + 'scripts') or
-                root.startswith(walk_dir + '.grunt')):
-            continue
-
         for filename in files:
             _, extension = os.path.splitext(filename)
             file_path = os.path.join(root, filename)
 
-            if extension.lower() in image_extensions:
+            if extension.lower() in known_extensions:
                 #: put all the static assets in set
-                files_on_disk.add(file_path)
+                parent = os.path.dirname(file_path).replace('./_site/', '')
+                files_on_disk.add(os.path.join(parent, filename))
 
-            if extension.lower() not in ['.html', '.md']:
+            if extension.lower() not in ['.html', '.htm', '.md']:
                 continue
 
-            #
-            # print('\t- file %s (full path: %s)' % (filename, file_path))
-            #
             with open(file_path, 'r') as original:
-                file_content = []
+                file_content = ''
                 for line_content in original.readlines():
-                    assets = find_assets(line_content)
+                    file_content += line_content
 
+                matches = re.findall(pattern, file_content, re.I | re.M | re.S)
+
+                if matches:
+                    [things_being_used.add(match) for match in matches]
+
+    unused = files_on_disk - things_being_used
+    pprint.pprint(unused, indent=2)
+    print(len(things_being_used))
+    print(len(files_on_disk))
+    print(len(unused))
+
+    for item in unused:
+        destination = os.path.join('/Users/sgourley/Projects/agrc.github.io/timeout/', item)
+        item = os.path.abspath(item)
+        if os.path.exists(item):
+            os.makedirs(destination)
+            shutil.move(item, destination)
 
 if __name__ == '__main__':
     # replace(sys.argv[1])
