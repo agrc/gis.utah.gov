e37b58b892d8a2821f27eb753dff75a0271645be
steveoh <sgourley@utah.gov>
2017-11-30 15:46:49 -0700

    udpate deps and switch to lozad
    closes #650

---
+++ bower_components/lozad/src/lozad.js
@@ -0,0 +1,71 @@
+const defaultConfig = {
+  rootMargin: '0px',
+  threshold: 0,
+  load(element) {
+    if (element.getAttribute('data-src')) {
+      element.src = element.getAttribute('data-src')
+    }
+    if (element.getAttribute('data-srcset')) {
+      element.srcset = element.getAttribute('data-srcset')
+    }
+    if (element.getAttribute('data-background-image')) {
+      element.style.backgroundImage = 'url(' + element.getAttribute('data-background-image') + ')'
+    }
+  }
+}
+
+function markAsLoaded(element) {
+  element.setAttribute('data-loaded', true)
+}
+
+const isLoaded = element => element.getAttribute('data-loaded') === 'true'
+
+const onIntersection = load => (entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      observer.unobserve(entry.target)
+
+      if (!isLoaded(entry.target)) {
+        load(entry.target)
+        markAsLoaded(entry.target)
+      }
+    }
+  })
+}
+
+export default function (selector = '.lozad', options = {}) {
+  const {rootMargin, threshold, load} = {...defaultConfig, ...options}
+  let observer
+
+  if (window.IntersectionObserver) {
+    observer = new IntersectionObserver(onIntersection(load), {
+      rootMargin,
+      threshold
+    })
+  }
+
+  return {
+    observe() {
+      const elements = document.querySelectorAll(selector)
+      for (let i = 0; i < elements.length; i++) {
+        if (isLoaded(elements[i])) {
+          continue
+        }
+        if (observer) {
+          observer.observe(elements[i])
+          continue
+        }
+        load(elements[i])
+        markAsLoaded(elements[i])
+      }
+    },
+    triggerLoad(element) {
+      if (isLoaded(element)) {
+        return
+      }
+
+      load(element)
+      markAsLoaded(element)
+    }
+  }
+}
---
+++ bower_components/lozad/test/index.js
@@ -0,0 +1,140 @@
+require('jsdom-global')()
+const assert = require('assert')
+const lozad = require('../dist/lozad.js')
+
+describe('lozad', () => {
+  describe('#lozad', () => {
+    it('should be a function', () => {
+      assert.equal('function', typeof lozad)
+    })
+  })
+
+  describe('#lozad return value', () => {
+    it('should be an object', () => {
+      const observer = lozad()
+      assert.equal('object', typeof observer)
+    })
+
+    it('should expose observe API', () => {
+      const observer = lozad()
+      assert.equal('function', typeof observer.observe)
+    })
+  })
+
+  describe('images inside viewport without class lozad', () => {
+    beforeEach(() => {
+      document.body.innerHTML = ''
+      const image = document.createElement('img')
+      image.dataset.src = Math.random()
+        .toString(36)
+        .substring(7)
+      document.body.appendChild(image)
+    })
+
+    it('should not load image', () => {
+      const observer = lozad()
+      observer.observe()
+      const image = document.getElementsByTagName('img')[0]
+      assert.equal(undefined, image.dataset.loaded)
+    })
+  })
+
+  describe('images inside viewport with class lozad', () => {
+    beforeEach(() => {
+      document.body.innerHTML = ''
+      const image = document.createElement('img')
+      image.dataset.src = Math.random()
+        .toString(36)
+        .substring(7)
+      image.setAttribute('class', 'lozad')
+      document.body.appendChild(image)
+    })
+
+    it('should not load an image till observe function is called', () => {
+      lozad()
+      const image = document.getElementsByTagName('img')[0]
+      assert.equal(undefined, image.dataset.loaded)
+    })
+
+    it('should load an image after observe function is called', () => {
+      const observer = lozad()
+      const image = document.getElementsByTagName('img')[0]
+      observer.observe()
+      assert.equal('true', image.dataset.loaded)
+      assert.equal(image.getAttribute('src'), image.dataset.src)
+    })
+  })
+
+  describe('images inside viewport with different class', () => {
+    beforeEach(() => {
+      document.body.innerHTML = ''
+      const image = document.createElement('img')
+      image.dataset.src = Math.random()
+        .toString(36)
+        .substring(7)
+      document.body.appendChild(image)
+    })
+
+    it('should load the image', () => {
+      const className = 'test-class'
+      const observer = lozad('.' + className)
+      const image = document.getElementsByTagName('img')[0]
+      image.setAttribute('class', className)
+      observer.observe()
+      assert.equal('true', image.dataset.loaded)
+      assert.equal(image.getAttribute('src'), image.dataset.src)
+    })
+  })
+
+  describe('images inside viewport with different attributes', () => {
+    beforeEach(() => {
+      document.body.innerHTML = ''
+      const image = document.createElement('img')
+      document.body.appendChild(image)
+    })
+
+    it('should load the image with data-srcset attribute', () => {
+      const dataSrcSetAttr = 'test-srcset'
+      const observer = lozad()
+      const image = document.getElementsByTagName('img')[0]
+      image.setAttribute('class', 'lozad')
+      image.setAttribute('data-srcset', dataSrcSetAttr)
+      observer.observe()
+      assert.equal('true', image.dataset.loaded)
+      assert.equal(image.getAttribute('srcset'), dataSrcSetAttr)
+    })
+
+    it('should load the image with data-background-image attribute', () => {
+      const bgImageAttr = 'test-bg-image'
+      const observer = lozad()
+      const image = document.getElementsByTagName('img')[0]
+      image.setAttribute('class', 'lozad')
+      image.setAttribute('data-background-image', bgImageAttr)
+      observer.observe()
+      assert.equal('true', image.dataset.loaded)
+      assert.equal(image.style.backgroundImage, 'url(' + bgImageAttr + ')')
+    })
+  })
+
+  describe('public API functions', () => {
+    beforeEach(() => {
+      document.body.innerHTML = ''
+      const image = document.createElement('img')
+      image.dataset.src = Math.random()
+        .toString(36)
+        .substring(7)
+      document.body.appendChild(image)
+    })
+
+    it('should load image via triggerLoad function', () => {
+      const dataSrcSetAttr = 'test-srcset'
+      const observer = lozad()
+      const image = document.getElementsByTagName('img')[0]
+      image.setAttribute('class', 'lozad')
+      image.setAttribute('data-srcset', dataSrcSetAttr)
+      observer.triggerLoad(image)
+      assert.equal('true', image.dataset.loaded)
+      assert.equal(image.getAttribute('src'), image.dataset.src)
+    })
+  })
+})
