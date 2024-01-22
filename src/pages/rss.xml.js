05f10439e0a6955a34c7d9d40f5a98df35dd6652
steveoh <sgourley@utah.gov>
2024-01-22 12:39:01 -0700

    fix: link pubDate with post date

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -10,6 +10,7 @@ export async function GET(context) {
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
+      pubDate: post.data.date,
       link: `/blog/${post.slug}/`,
     })),
   });
