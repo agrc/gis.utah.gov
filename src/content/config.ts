9b26c76f60590ac4387df9a68ab74838b49d1254
steveoh <sgourley@utah.gov>
2024-01-22 11:53:04 -0700

    fix: allow jekyll blog posts to render

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,12 +3,16 @@ import { defineCollection, z } from 'astro:content';
 const blog = defineCollection({
   // Type-check frontmatter using a schema
   schema: z.object({
+    // author: z.object({
+    //   display_name: z.string(),
+    // }),
+    // tags: z.array(z.string()).optional(),
     title: z.string(),
-    description: z.string(),
+    // description: z.string().optional(),
     // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
+    date: z.coerce.date(),
+    updated: z.coerce.date().optional(),
+    // categories: z.array(z.string()).optional(),
   }),
 });
 
