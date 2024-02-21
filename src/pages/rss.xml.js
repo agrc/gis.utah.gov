7315c63c84d0a21ed14c98cb384bf5e1b5b673a5
stdavis <stdavis@utah.gov>
2024-02-21 07:45:34 -0700

    fix: fix rss build

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,6 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
 export async function GET(context) {
   const posts = await getCollection('blog');
@@ -9,7 +9,7 @@ export async function GET(context) {
     description: SITE_DESCRIPTION,
     site: context.site,
     items: posts.map((post) => ({
-      ...post.data,
+      title: post.data.title,
       pubDate: post.data.date,
       link: `/blog/${post.slug}/`,
     })),
