ed92671e35aae6ea3593139f84141bab0cc39a7c
stdavis <stdavis@utah.gov>
2024-02-21 07:20:42 -0700

    feat: add blog post author, category, and tags links

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,19 +3,17 @@ import { defineCollection, z } from 'astro:content';
 const blog = defineCollection({
   // Type-check frontmatter using a schema
   schema: z.object({
-    // author: z.object({
-    //   display_name: z.string(),
-    // }),
-    // tags: z.array(z.string()).optional(),
-    title: z.string(),
+    author: z.object({
+      display_name: z.string(),
+    }),
     // description: z.string().optional(),
-    // Transform string to Date object
+    categories: z.array(z.string()).optional(),
     date: z.coerce.date(),
-    updated: z.coerce.date().optional(),
-    published: z.boolean().optional().default(true),
-    lastUpdated: z.date().optional(),
     estimatedReadTime: z.string().optional(),
-    // categories: z.array(z.string()).optional(),
+    lastUpdated: z.date().optional(),
+    published: z.boolean().optional().default(true),
+    tags: z.array(z.string()).optional(),
+    title: z.string(),
   }),
 });
 
