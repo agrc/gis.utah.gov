dcee9f45edfc0114070f93d33813a4e7d714a437
steveoh <sgourley@utah.gov>
2024-02-22 13:11:42 -0700

    chore: migration for blog post downloads

--- src/migrations/liquid-downloads.mjs
+++ src/migrations/liquid-downloads.mjs
@@ -1,4 +1,13 @@
-import { appendFileSync, copyFileSync, existsSync, mkdirSync, readFileSync, readdirSync, writeFileSync } from 'fs';
+import {
+  appendFileSync,
+  copyFileSync,
+  existsSync,
+  mkdirSync,
+  readFileSync,
+  readdirSync,
+  renameSync,
+  writeFileSync,
+} from 'fs';
 import matter from 'gray-matter';
 import { basename, dirname, join } from 'path';
 
@@ -7,10 +16,11 @@ const filenames = readdirSync(blogPostFolder);
 
 const regex = /{%\s?link (downloads\/.*)\s?%}/gim;
 const migratedFiles = new Set();
+const convertToMdx = new Set();
 
 for (const filename of filenames) {
   let file = readFileSync(`${blogPostFolder}/${filename}`, 'utf8');
-  const { data: frontMatter } = matter(file);
+  const { data: frontMatter, content } = matter(file);
 
   let matches;
   let changed = false;
@@ -20,25 +30,27 @@ for (const filename of filenames) {
     continue;
   }
 
+  let downloadCount = 0;
   while ((matches = regex.exec(file)) !== null) {
     console.log(`${filename}: ${matches[0]}`);
     // find the image from the old site
     const oldPath = join('..', '..', matches[1].trim());
+
     if (!existsSync(oldPath)) {
+      changed = true;
+
       file = file.replace(matches[0], 'deleted');
-      writeFileSync(`${blogPostFolder}/${filename}`, file);
 
       continue;
     }
 
     changed = true;
-    const imageName = basename(matches[1].trim());
 
-    // move it to the new site images folder creating a folder based on filename
-    // remove file path and extension from filename using fs
+    convertToMdx.add(`${blogPostFolder}/${filename}`);
+    const assetName = basename(matches[1].trim());
 
     const folderName = basename(filename, '.md');
-    const newPath = join('..', 'downloads', 'pillar-blog', folderName, imageName.toLowerCase());
+    const newPath = join('..', 'downloads', 'pillar-blog', folderName, assetName.toLowerCase());
 
     console.log(oldPath, newPath);
 
@@ -62,5 +74,6 @@ for (const filename of filenames) {
 
 mkdirSync('./log/', { recursive: true });
 migratedFiles.forEach((path) => appendFileSync('./log/blog-migration-downloads.log', path + '\n'));
+convertToMdx.forEach((path) => renameSync(path, `${path}x`));
 
 process.exit(0);
