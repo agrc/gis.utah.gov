73d770f687593783d4852c60a1242f6e493fe828
Zach Beck <zbeck@utah.gov>
2024-03-06 11:01:23 -0700

    PSAP boundaries

--- src/data/downloadMetadata.ts
+++ src/data/downloadMetadata.ts
@@ -1848,8 +1848,8 @@ export const dataPages: DownloadMetadata = {
   'Utah Public Safety Answering Point Boundaries': {
     itemId: '34f4c65991ce4d4299d3957efa35bf41',
     name: 'Utah Public Safety Answering Point Boundaries',
-    featureServiceId: null,
-    openSgid: '',
+    featureServiceId: 'PSAPBoundaries',
+    openSgid: 'society.public_safety_answering_point_boundaries',
     layerId: 0,
   },
   'Utah Public Safety Answering Point Locations': {
