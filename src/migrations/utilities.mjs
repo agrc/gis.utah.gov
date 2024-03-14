eef29ac058192813f60a076caf71cdda50b97233
stdavis <stdavis@utah.gov>
2024-03-14 15:49:07 -0600

    feat: validateOpenDataUrl utility function

--- src/migrations/utilities.mjs
+++ src/migrations/utilities.mjs
@@ -1,5 +1,6 @@
 import { readdirSync, readFileSync, writeFileSync } from 'fs';
 import matter from 'gray-matter';
+import ky from 'ky';
 import { stringify } from 'yaml';
 
 export function updateBlogPosts(update) {
@@ -17,4 +18,43 @@ export function updateBlogPosts(update) {
 
     writeFileSync(`${blogPostFolder}/${filename}`, newFileContent);
   }
+}
+
+export async function validateOpenDataUrl(url) {
+  const domainToOrg = {
+    'data.wfrc.org': 'wfrc',
+    'opendata.gis.utah.gov': 'utah',
+    'data-uplan.opendata.arcgis.com': 'uplan',
+  };
+
+  const match = /https:\/\/.*?\/datasets\/((?<org>.*)::)?(?<slug>.*?)(\/|$)/.exec(url);
+  const slug = match.groups.slug;
+  let org = match.groups.org;
+
+  const domain = new URL(url).hostname;
+  if (!Object.keys(domainToOrg).includes(domain)) {
+    throw new Error(`Unknown domain: ${domain}`);
+  }
+
+  if (!org) {
+    org = domainToOrg[domain];
+  }
+  const openDataQuery = 'https://opendata.arcgis.com/api/v3/datasets';
+  const response = await ky(openDataQuery, {
+    searchParams: {
+      'fields[datasets]': 'slug,boundary,extent,recordCount,searchDescription,statistics',
+      'filter[slug]': `${org}::${slug}`,
+    },
+  }).json();
+
+  if (response.data.length === 0) {
+    return {
+      valid: false,
+    };
+  }
+
+  return {
+    valid: true,
+    data: response.data[0],
+  };
 }
