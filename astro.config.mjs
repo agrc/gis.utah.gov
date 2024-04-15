7d6e0b2340fe957c1dd60f2b324cf319a7ab49c5
steveoh <sgourley@utah.gov>
2024-04-15 10:17:33 -0600

    chore: move config to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,27 +8,48 @@ import { execSync } from 'child_process';
 import rehypeExternalLinks from 'rehype-external-links';
 import externalLinkConfig from './plugins/externalLinks';
 
-import partytown from "@astrojs/partytown";
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.BRANCH === 'main' ? process.env.URL : process.env.DEPLOY_URL || 'https://gis.utah.gov',
   image: {
-    domains: ['gis.utah.gov']
+    domains: ['gis.utah.gov'],
   },
-  integrations: [mdx(), sitemap(), tailwind({
-    applyBaseStyles: false
-  }), react(), {
-    name: 'pagefind',
-    hooks: {
-      'astro:build:done': () => {
-        execSync('npx pagefind --site dist', {
-          stdio: [process.stdin, process.stdout, process.stderr]
-        });
-      }
-    }
-  }, metaTags(), partytown()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    react(),
+    {
+      name: 'pagefind',
+      hooks: {
+        'astro:build:done': () => {
+          execSync('npx pagefind --site dist', {
+            stdio: [process.stdin, process.stdout, process.stderr],
+          });
+        },
+      },
+    },
+    metaTags(),
+    partytown({
+      config: {
+        debug: true,
+        forward: [
+          'dataLayer.push',
+          'analytics.track',
+          'analytics.identify',
+          'analytics.group',
+          'analytics.reset',
+          'analytics.user',
+          'analytics.alias',
+        ],
+      },
+    }),
+  ],
   markdown: {
-    rehypePlugins: [[rehypeExternalLinks, externalLinkConfig]]
-  }
+    rehypePlugins: [[rehypeExternalLinks, externalLinkConfig]],
+  },
 });
